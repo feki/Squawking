9c497c7ec94b26518323e71ccbe3ea553b15fa27
Jakub Barbierik <jakub.barbierik2@gmail.com>
2012-11-05 13:12:28 +0100

    bootstrap style layouts(application, _navbar, _footer) added

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 
   gem 'twitter-bootstrap-rails'
+
+  gem 'slim'
 end
 
 group :development do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def app_name
+    "SquawKING"
+  end
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = app_name 
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
