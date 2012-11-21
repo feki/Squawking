3875abf5792494d27072e7764128960ba3e83eb1
Jakub Barbierik <jakub.barbierik2@gmail.com>
2012-11-21 10:20:15 +0100

    projects views styling

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   # GET /projects.json
   def index
     # @projects = Project.all
-    @projects = Project.paginate(page: params[:page])
+    @projects = Project.paginate(page: params[:page], per_page: 10)
 
     respond_to do |format|
       format.html # index.html.erb
